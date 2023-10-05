dce8ef4496899aa3f421bf204383fd5107499319
amanda <amanda.santana@ccc.ufcg.edu.br>
2023-10-05 13:26:38 -0300

    corrige botoes home bibliotecario

--- biblioconex/src/containers/Perfis/Bibliotecario.js
+++ biblioconex/src/containers/Perfis/Bibliotecario.js
@@ -5,27 +5,27 @@ function Bibliotecario() {
         <div className='Bibliotecario-contents'>
 
             <h1>Cadastrar livros</h1>
-            <div className='Contents'>
+            <div className='Contents-perfis'>
                 <input placeholder="ISBN"></input>
                 <div className='botao'>Cadastrar</div>
             </div>
 
             <h1>Turma</h1>
-            <div className='Contents'>
+            <div className='Contents-perfis'>
                 <div className='botao'>Cadastrar turma</div>
                 <div className='botao'>Alunos por turma</div>
                 <div className='botao'>Alterar dados</div>
             </div>
 
             <h1>Aluno</h1>
-            <div className='Contents'>
+            <div className='Contents-perfis'>
                 <div className='botao'>Cadastrar aluno</div>
                 <div className='botao'>Dados do aluno</div>
                 <div className='botao'>Livros emprestados</div>
             </div>
 
             <h1>Professor</h1>
-            <div className='Contents'>
+            <div className='Contents-perfis'>
             <div className='botao'>Cadastrar professor</div>
             </div>
         </div >
