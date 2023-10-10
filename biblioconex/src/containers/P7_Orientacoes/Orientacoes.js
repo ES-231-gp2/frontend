ffd48f0014a533889ad37662db4e577e119d4edc
Lucas <lucas.soares@ccc.ufcg.edu.br>
2023-10-09 22:29:36 -0300

    Outros ajustes em orientacoes

--- biblioconex/src/containers/P7_Orientacoes/Orientacoes.js
+++ biblioconex/src/containers/P7_Orientacoes/Orientacoes.js
@@ -5,13 +5,13 @@ function Orientacao1() {
         <div className='Orientacao-content'>
 
             <h1>Saiba como <span className='detalhe-rosa'>reservar um livro</span> ou entrar para a <span className='detalhe-amarelo'>fila de espera</span></h1>
-            <h4>Blog com instruções sobre como fazer a reserva do livro pelo site.</h4>
+            <p className='Orientacao-text'>Blog com instruções sobre como fazer a reserva do livro pelo site. Blog com instruções sobre como fazer a reserva do livro pelo site.</p>
 
-            <img src='/children-bookshell.png'></img>
+            <img className='Orientacao-img' src='/children-lendo.png' alt='Duas crianças lendo livros ao lado de uma estante de livros enquanto uma escolhe um para ler'></img>
 
-            <h4>Blog com instruções sobre como entrar para a fila de reserva de um livro que já está reservado.</h4>
-            <h4>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</h4>
-
+            <p className='Orientacao-text'>Blog com instruções sobre como entrar para a fila de reserva de um livro que já está reservado. Blog com instruções sobre como entrar para a fila de reserva de um livro que já está reservado.</p>
+            <p className='Orientacao-text'>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo. Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</p>
+ 
         </div>
     )
 }
@@ -20,9 +20,9 @@ function Orientacao2() {
     return (
         <div className='Orientacao-content'>
             <h1>Saiba como fazer o pedido ou sugerir um livro</h1>
-            <h4>Blog com instruções sobre como fazer pedidos ou sugestões de novos livros para o acervo da biblioteca.</h4>
-            <img src="/childrens_with_books1.png" />
-            <h4>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</h4>
+            <p className='Orientacao-text'>Blog com instruções sobre como fazer pedidos ou sugestões de novos livros para o acervo da biblioteca.</p>
+            <img className='Orientacao-img' src="/childrens_with_books1.png" />
+            <p className='Orientacao-text'>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</p>
 
         </div>
     )
@@ -32,10 +32,10 @@ function Orientacao3() {
     return (
         <div className='Orientacao-content'>
             <h1>Como funciona nossos prazos de entrega e cuidados com o livro</h1>
-            <h4>Blog com instruções sobre prazos de entrega e como o aluno pode ser notificado.</h4>
-            <img src="./childrens_with_books2.png" />
-            <h4>Texto explicativo de como o aluno deve cuidar do livro e dicas de cuidado.</h4>
-            <h4>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</h4>
+            <p className='Orientacao-text'>Blog com instruções sobre prazos de entrega e como o aluno pode ser notificado.</p>
+            <img className='Orientacao-img' src="./childrens_with_books2.png" />
+            <p className='Orientacao-text'>Texto explicativo de como o aluno deve cuidar do livro e dicas de cuidado. Texto explicativo de como o aluno deve cuidar do livro e dicas de cuidado. Texto explicativo de como o aluno deve cuidar do livro e dicas de cuidado.</p>
+            <p className='Orientacao-text'>Texto descritivo, usando imagens se necessário para ilustrar algum passo a passo.</p>
 
         </div>
     )
