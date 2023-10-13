d1eea1d3ee6c17752fdb7b098c0ca929ca848af2
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-10-13 02:42:08 -0300

    comenta footer e add botao volta ao topo

--- biblioconex/src/App.js
+++ biblioconex/src/App.js
@@ -1,39 +1,64 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
 import logo from './imagens/logo.svg';
 import lupa from './imagens/lupa.svg';
 import './App.css';
 import './App-header.css';
-import './App-footer.css'
 
 import Home from './containers/P1_Home/Home';
-import Login from './containers/P2_Login/Login'
+import Login from './containers/P2_Login/Login';
 import Bibliotecario from './containers/Perfis/Bibliotecario';
-import Aluno from './containers/Perfis/Aluno'
+import Aluno from './containers/Perfis/Aluno';
 import Catalogo from './containers/Catalogo/Catalogo';
-import Livros_Aluno from './containers/Livros_Aluno/livros_aluno'
+import Livros_Aluno from './containers/Livros_Aluno/livros_aluno';
 import Cadastro from './containers/Cadastro/Cadastro';
-import {Orientacao1, Orientacao2, Orientacao3} from './containers/P7_Orientacoes/Orientacoes'
+import { Orientacao1, Orientacao2, Orientacao3 } from './containers/P7_Orientacoes/Orientacoes';
 import Turmas from './containers/Turmas/Turmas';
 import Alunos from './containers/Alunos/Alunos';
 import Professor from './containers/Perfis/Professor';
 
 function App() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 20) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  // Função para rolar para o topo da página
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Rolagem suave
+    });
+  };
+
   return (
     <div className="App">
-      <BrowserRouter> {/* Mova o BrowserRouter para envolver toda a aplicação */}
+      <BrowserRouter>
         <header className="App-header">
-          <Link to="/"> {/* Adicione o Link aqui */}
+          <Link to="/">
             <img src={logo} className="App-header-logo" alt="logo" />
           </Link>
-          <div className='Header-inputs'>
-            <div className='App-header-entrar'>
-              <Link to="/login" className='Custom-link-button'>
+          <div className="Header-inputs">
+            <div className="App-header-entrar">
+              <Link to="/login" className="Custom-link-button">
                 Entrar
               </Link>
             </div>
-            <input placeholder='Pesquisar por...' className='App-header-barraDePesquisa'></input>
-            <img src={lupa} className='App-header-lupa' />
+            <input placeholder="Pesquisar por..." className="App-header-barraDePesquisa" />
+            <img src={lupa} className="App-header-lupa" alt="Lupa" />
           </div>
         </header>
 
@@ -44,8 +69,8 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/perfil" element={<Bibliotecario />} />
               <Route path="/aluno" element={<Aluno />} />
-              <Route path='/turmas' element={<Turmas />} />
-              <Route path='/alunos' element={<Alunos />} />
+              <Route path="/turmas" element={<Turmas />} />
+              <Route path="/alunos" element={<Alunos />} />
               <Route path="/catalogo" element={<Catalogo />} />
               <Route path="/livros_aluno" element={<Livros_Aluno />} />
               <Route path="/cadastro" element={<Cadastro />} />
@@ -53,15 +78,16 @@ function App() {
               <Route path="/Orientacao1" element={<Orientacao1 />} />
               <Route path="/Orientacao2" element={<Orientacao2 />} />
               <Route path="/Orientacao3" element={<Orientacao3 />} />
-              <Route path="/livros_por_aluno" element={<Livros_Aluno/>}/>
+              <Route path="/livros_por_aluno" element={<Livros_Aluno />} />
             </Route>
           </Routes>
         </div>
 
-        <footer className="App-footer">
-          <img src={logo} className="App-footer-logo" alt="logo"></img>
-        </footer>
-      </BrowserRouter> {/* Feche o BrowserRouter aqui */}
+        {showScrollButton && (
+          <button class="scrollToTopButton" onClick={scrollToTop}><img src="ScrollTop.svg"/></button>
+        )}
+
+      </BrowserRouter>
     </div>
   );
 }
