214d9dbbe74bd5a522e425542ea67e8b1dfe7137
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-10-13 01:40:18 -0300

    add botao de alterar dados do aluno

--- biblioconex/src/containers/Perfis/Aluno.js
+++ biblioconex/src/containers/Perfis/Aluno.js
@@ -1,14 +1,46 @@
+import { useState } from "react";
 import "./aluno.css"
 
 function Aluno() {
+
+    const [isEditing, setEditing] = useState(false);
+    const [name, setName] = useState('Fulano da Silva');
+    const [classInfo, setClassInfo] = useState('7° ano B');
+    const [shift, setShift] = useState('Tarde');
+  
+    const handleEditClick = () => {
+      setEditing(true);
+    };
+  
+    const handleSaveClick = () => {
+      setEditing(false);
+      // Você pode adicionar aqui a lógica para salvar as informações em algum lugar, como em um banco de dados.
+    };
+
     return (
         <div className='Aluno-contents'>
-            <h1 class='titulo'>MEU PERFIL</h1>
-            <div class='info'>
-            <p>Nome completo</p>
-            <p>Série/ano e turma</p>
-            <p>Turno</p>
-            </div>
+            <h1 className='titulo'>{isEditing ? 'EDITAR PERFIL' : 'MEU PERFIL'}</h1>
+      <div className='info'>
+        {isEditing ? (
+          <>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" value={classInfo} onChange={(e) => setClassInfo(e.target.value)} />
+            <input type="text" value={shift} onChange={(e) => setShift(e.target.value)} />
+          </>
+        ) : (
+          <>
+            <p>Nome completo: {name}</p>
+            <p>Série/ano e turma: {classInfo}</p>
+            <p>Turno: {shift}</p>
+          </>
+        )}
+      </div>
+
+      {isEditing ? (
+        <div className='botao' style={{ alignSelf: "center", background: "#FFC107", marginBottom: '50px' }} onClick={handleSaveClick}>Salvar</div>
+      ) : (
+        <div className='botao' style={{ alignSelf: "center", background: "#FFC107", marginBottom: '50px' }} onClick={handleEditClick}>Alterar dados</div>
+      )}
             
                 
             <h2 className='titulo-secao-aluno'>TEXTO DA TURMA</h2>
@@ -68,6 +100,8 @@ function Aluno() {
                 </div>
                 {/* falta o sistema de páginas */}
             </div>
+
+            
         </div>
     );
 }
