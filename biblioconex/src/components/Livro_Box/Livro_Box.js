98e778e3150937df4080289224a87d31ab1bf7d1
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-10-13 00:43:51 -0300

    corrige a tela de livros por aluno e mlhora o componente livro_box

--- biblioconex/src/components/Livro_Box/Livro_Box.js
+++ biblioconex/src/components/Livro_Box/Livro_Box.js
@@ -1,27 +1,22 @@
 import React from "react";
 import "./Livro_Box.css"
 
-
-export default function(props) {
-    return(
-       
+export default function LivroBox(props) {
+    return (
         <div className='Contents-livros'>
             <p className='Titulo'>{props.titulo}</p>
             <div>
                 <div className='Livro-img-sinopse'>
-                    <img className='Livro' src={props.img_src}></img>
-                    <p className='Sinopse'>
-                    {props.sinopse}
-                    </p>
+                    <img className='Livro' src={props.imgSrc} alt={props.imgAlt} />
+                    <p className='Sinopse'>{props.sinopse}</p>
                 </div>
-                <div className='Botao-reserva'> Devolver</div>
-                <div className='Botao-reserva'> Resenha</div>
+                {props.showDevolverButton && <div className='Botao-reserva'>{props.botao1}</div>}
+                {props.showResenhaButton && <div className='Botao-reserva'>{props.botao2}</div>}
             </div>
             <div className='Linha-inferior'>
-                <p>1 2 3 4 5 ...</p><img className='Botao-seta' src='seta.svg'></img>
+                <p>{props.pageNumbers}</p>
+                <img className='Botao-seta' src={props.setaImgSrc} alt={props.setaImgAlt} />
             </div>
         </div>
-    
-        
-    )
+    );
 }
--- biblioconex/src/containers/Livros_Aluno/livros_aluno.js
+++ biblioconex/src/containers/Livros_Aluno/livros_aluno.js
@@ -1,11 +1,31 @@
-import './livros_aluno.css'
+import './livros_aluno.css';
 import Livro_Box from '../../components/Livro_Box/Livro_Box';
 
-export default function() {
+export default function LivrosAluno() {
     return (
         <div className='Catalogo-contents'>
-            <Livro_Box img_src="Capa _Magalice.png" titulo="Magalice" sinopse="Livro da magali" />
-            <Livro_Box img_src="childrens_with_books2.png" titulo="Livro da biblioconex ;)" sinopse="Como fazer um sistema de bibliotecas" />
+            <h1 className='titulo-livros-aluno'>LIVROS</h1>
+            <div className='info'>Nome do aluno</div>
+            <Livro_Box
+                titulo="Magalice no país das melancias"
+                imgSrc="/Capa _Magalice.png"
+                imgAlt="Descrição da imagem"
+                sinopse="Texto da sinopse do livro..."
+                showDevolverButton={true}
+                botao1='Devolver'
+                showResenhaButton={true}
+                botao2='Resenha'
+            />
+            <Livro_Box
+                titulo="Livro da BiblioConex ;)"
+                imgSrc="/childrens_with_books2.png"
+                imgAlt="Descrição da imagem"
+                sinopse="Texto da sinopse do livro..."
+                showDevolverButton={true}
+                botao1='Devolver'
+                showResenhaButton={true}
+                botao2='Resenha'
+            />
         </div>
-    )
+    );
 }
