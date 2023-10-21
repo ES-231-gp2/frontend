ebcbbb0dc1b8836e63e3c7f3152384f1665b8d5a
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-10-20 22:36:53 -0300

    add a função de deletar uma turma

--- biblioconex/src/components/Card/Card.js
+++ biblioconex/src/components/Card/Card.js
@@ -1,12 +1,37 @@
-import './Card.css'
+import React, { useState } from 'react';
+import './Card.css';
 
-export default function(props) {
+export default function Card(props) {
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+    const handleDeleteClick = () => {
+        setShowDeleteConfirmation(true);
+    };
+
+    const handleConfirmDelete = () => {
+        // Chame a função onDelete passada como prop para lidar com a exclusão
+        if (props.onDelete) {
+            props.onDelete(props.nome);
+        }
+    };
+
     return (
         <div className='Turma'>
-            <p className='Nome-turma'>{props.nome}</p>
-            <div className='Botao-excluir'>
-                X
-            </div>
+            {showDeleteConfirmation && (
+                <div className='Exclusao-confirmacao'>
+                    <p>Tem certeza de que deseja excluir a turma "{props.nome}"?</p>
+                    <button onClick={handleConfirmDelete}>Sim</button>
+                    <button onClick={() => setShowDeleteConfirmation(false)}>Cancelar</button>
+                </div>
+            )}
+            {!showDeleteConfirmation && (
+                <>
+                    <p className='Nome-turma'>{props.nome}</p>
+                    <div className='Botao-excluir' onClick={handleDeleteClick}>
+                        X
+                    </div>
+                </>
+            )}
         </div>
     );
 }
