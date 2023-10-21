c62af2e42b26c25fecd45aab32e51fe68e8727ad
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-10-21 00:12:33 -0300

    add mensagem de sucesso ao cadastrar e melhora o componente

--- biblioconex/src/containers/Cadastro/Cadastro.js
+++ biblioconex/src/containers/Cadastro/Cadastro.js
@@ -1,20 +1,96 @@
-import './Cadastro.css'
+import React, { useState } from 'react';
+import './Cadastro.css';
 
 export default function () {
+    const [userData, setUserData] = useState({
+        nome: '',
+        tipoUsuario: '',
+        turma: '',
+        email: '',
+        senha: '',
+    });
+
+    const [cadastroSucesso, setCadastroSucesso] = useState(false); 
+    const [redirectToLogin, setRedirectToLogin] = useState(false);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const handleCadastroClick = () => {
+        // Lógica de cadastro simulada
+        setTimeout(() => {
+            setCadastroSucesso(true);
+            setRedirectToLogin(true);
+            setUserData({
+                nome: '',
+                tipoUsuario: '',
+                turma: '',
+                email: '',
+                senha: '',
+            });
+        }, 5000);
+    };
+
+    if (redirectToLogin) {
+        window.location.href = '/login';
+    }
+
     return (
         <div className='Cadastro-contents'>
             <h1>FAÇA PARTE DA NOSSA <span className='biblioteca-rosa'>BIBLIOTECA</span></h1>
             <div className='Form-contents-cadastro'>
-                <input placeholder='Nome Completo'></input>
-                <input placeholder='Tipo de Usuário'></input>
-                <input placeholder='Turma do Aluno'></input>
-                <input placeholder='E-mail'></input>
-                <input placeholder='Senha'></input>
-                <div className='botao-cadastro'>
+                {cadastroSucesso && (
+                    <div className='mensagem-sucesso'>
+                        Cadastro realizado com sucesso!
+                    </div>
+                )}
+                <input
+                    type="text"
+                    name="nome"
+                    placeholder='Nome Completo'
+                    value={userData.nome}
+                    onChange={handleInputChange}
+                />
+                <select
+                    name="tipoUsuario"
+                    value={userData.tipoUsuario}
+                    onChange={handleInputChange}
+                >
+                    <option value="">Selecione o Tipo de Usuário</option>
+                    <option value="aluno">Aluno</option>
+                    <option value="professor">Professor</option>
+                </select>
+                <select
+                    name="turma"
+                    value={userData.turma}
+                    onChange={handleInputChange}
+                >
+                    <option value="">Selecione a Turma</option>
+                    <option value="Turma A">Turma A</option>
+                    <option value="Turma B">Turma B</option>
+                    <option value="Turma C">Turma C</option>
+                </select>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder='E-mail'
+                    value={userData.email}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="password"
+                    name="senha"
+                    placeholder='Senha'
+                    value={userData.senha}
+                    onChange={handleInputChange}
+                />
+                <div className='botao-cadastro' onClick={handleCadastroClick}>
                     Cadastrar
                 </div>
+                
             </div>
         </div>
-    )
-    //TODO: Criar alguma ação para o botão de cadastro, aparecer uma mensagem ou algo do tipo
+    );
 }
