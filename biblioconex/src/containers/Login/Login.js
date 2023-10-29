3aece72889381e2d580f624bfa0bde5fd5ce8840
Jônatas Tavares dos Santos <jonatas.santos@ccc.ufcg.edu.br>
2023-10-29 19:07:03 -0300

    Integra login

--- biblioconex/src/containers/Login/Login.js
+++ biblioconex/src/containers/Login/Login.js
@@ -1,19 +1,56 @@
-import { Link } from 'react-router-dom'
-import './Login.css'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
-export default function () {
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_ENV,
+});
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const user = await instance.get(`/login?login=${email}&senha=${senha}`);
+            localStorage.setItem("user", JSON.stringify(user));
+            if (user.tipo_usuario === "BIBLIOTECARIO") {
+                navigate('/bibliotecario');
+            } else {
+                navigate('/catalogo');
+            }
+        } catch (error) {
+            setError("Credenciais inválidas. Tente novamente.")
+        }
+    };
+
     return (
         <div className='Login-contents'>
             <h1>ACESSO À <span className='biblioteca-amarelo'>BIBLIOTECA</span></h1>
             <div className='Form-contents'>
-                <input placeholder='E-mail'></input>
-                <input placeholder='Senha'></input>
+                <input
+                    placeholder='E-mail'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type='password'
+                    placeholder='Senha'
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                />
                 <div className='botao-login'>
-                    <Link to="/bibliotecario" className='Custom-link-button'>
+                    <button onClick={handleLogin} className='Custom-link-button'>
                         Entrar
-                    </Link>
+                    </button>
                 </div>
+                {error && <p className='error-message'>{error}</p>}
             </div>
         </div>
-    )
+    );
 }
+
+export default Login;
